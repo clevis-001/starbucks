0ebf8dbe5931bcbcf3bea1c70a3a5144905c345a
clevis-001 <leison0802@gmail.com>
2021-09-01 21:56:57 +0900

    공통모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function() {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function() {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 //검색바 애니메이션
 
 const badgeEl=document.querySelector('header .badges');
@@ -147,5 +130,3 @@ spyEls.forEach(function(spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear=document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
